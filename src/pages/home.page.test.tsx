4d83da8783064ecd1fa9d20b804dda996400cf15
Jesus Alvarez <jesusalvarezaguilar94@gmail.com>
2023-08-27 12:23:07 +0200

    Add home-page test and css

--- src/pages/home.page.test.tsx
+++ src/pages/home.page.test.tsx
@@ -1,14 +1,22 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-
+import { MemoryRouter } from 'react-router-dom';
 import HomePage from './home.page';
 
 describe('Given the component HomePage', () => {
   describe('When it is rendered', () => {
-    render(<HomePage></HomePage>);
-    test('The string Top Players List should be in the document', () => {
-      const element = screen.getByRole('heading');
-      expect(element).toBeInTheDocument();
+    let button: HTMLButtonElement;
+    beforeEach(() => {
+      render(
+        <MemoryRouter>
+          <HomePage></HomePage>
+        </MemoryRouter>
+      );
+      button = screen.getByRole('button');
+    });
+
+    test('the component should be in the document', () => {
+      expect(button).toBeInTheDocument();
     });
   });
 });
--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,12 +1,22 @@
+import { Link } from 'react-router-dom';
 import { Home } from '../components/home/home';
 import styles from './home.page.module.scss';
 
 function HomePage() {
   return (
-    <>
+    <div className={styles['home-page']}>
       <h2 className={styles['title']}>Top Players List</h2>
+
       <Home></Home>
-    </>
+      <Link
+        role="button"
+        id="buttonPlayer"
+        className={styles['buttonPlayer']}
+        to={'/'}
+      >
+        Add Player
+      </Link>
+    </div>
   );
 }
 export default HomePage;
